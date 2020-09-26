b2fc809be544656fd5c3b7688e5b01c09f15a907
Blake-Cash <35136335+Blake-Cash@users.noreply.github.com>
2020-09-26 00:33:17 -0700

    Functional but very bad

--- testfile.py
+++ testfile.py
@@ -1,16 +1,33 @@
+#Selenium for Web Driving
 import selenium
 from selenium import webdriver
 
+#Time for waiting to redirect
 import time
+
+#Requests for querying holotools
 import requests
 
-url = "https://api.holotools.app/v1/live"
+#Random for picking random active stream
+import random
 
-def navigateTo():
+url = "https://api.holotools.app/v1/live?hide_channel_desc=1&max_upcoming_hours=1&lookback_hours=0"
+
+def getDestination():
+    response = requests.get(url)
+    parsedResponse = (response.json())
+    liveChannels = (parsedResponse["live"])
+    amtOfChannelsLive = len(liveChannels)
+    selectedChannelNumber = random.randint(0, amtOfChannelsLive-1)
+    selectedChannel = liveChannels[selectedChannelNumber]
+    return selectedChannel['yt_video_key']
+
+def navigateTo(destination):
     driver = webdriver.Chrome()
-    driver.get('https://twitter.com/home')
+    driver.get('https://youtube.com/watch?v=' + destination)
+    time.sleep(3600)
 
-print ('Starting HoloTV')
-response = requests.get(url)
-print (response.json())
-navigateTo()
+print ('Starting HoloTV!')
+while True:
+    destination = getDestination()
+    navigateTo(destination)
